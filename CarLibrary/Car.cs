76d8758e9904673f8cab63bb492e07325f0cc31a
mette <mette@BOOK-LKBP3NGMUK>
2023-02-27 14:32:06 +0100

    Add project files.

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,65 @@
+﻿using System.Transactions;
+using System.Xml.Linq;
+
+namespace CarLibrary
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string? Model { get; set; }
+		public int Price { get; set; }
+		public string? LicensePlate { get; set; }
+
+
+		public override string ToString()
+		{
+			return Id + " " + Model + " " + Price + " " + LicensePlate;
+		}
+
+		/// <summary>
+		/// ValidateModel() throws 1 out of 2 Exceptions, if Model is null or less 4 characters. 
+		/// </summary>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public void ValidateModel()
+		{
+			if (Model == null)
+				throw new ArgumentNullException("Invalid model. Must not be null. Your input: " + Model);
+			if (Model.Length <= 4)
+				throw new ArgumentException("Invalid model. Must be at least 4 characters. Your input: " + Model);
+		}
+
+		/// <summary>
+		/// ValidatePrice() throws an Exception, if Price is less than 1.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public void ValidatePrice()
+		{
+			if (Price < 1)
+				throw new ArgumentOutOfRangeException("Invalid price. Must be at least 1. Your input: " + Price);
+		}
+
+		/// <summary>
+		/// ValidateLicensePlate() throws 1 out of 2 Exceptions, if LicensePlate is null, less than 2 or larger than 7. 
+		/// </summary>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public void ValidateLicensePlate()
+		{
+			if (LicensePlate == null)
+				throw new ArgumentNullException("Invalid license plate. Must not be null. Your input: " + LicensePlate);
+			if (LicensePlate.Length < 2 || LicensePlate.Length > 7)
+				throw new ArgumentOutOfRangeException("Invalid license plate. Must be between 2-7 characters. The length of your input: " + LicensePlate.Length);
+		}
+
+		/// <summary>
+		/// Call all validate methods collectively.   
+		/// </summary>
+		public void ValidateAll()
+		{
+			ValidateModel();
+			ValidatePrice();
+			ValidateLicensePlate();
+		}
+	}
+}
---
+++ CarLibraryTests/CarTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary.Tests
+{
+	[TestClass()]
+	public class CarTests
+	{
+		//ToStringTest()-case
+		Car validToString = new Car() { Id = 1, Model = "Model", Price = 2, LicensePlate = "AB12345" };
+
+		//ValidateModelTest()-cases
+		Car validModel = new Car() { Id = 2, Model = "Model", Price = 2, LicensePlate = "AB12345" };
+		Car invalidModelNull = new Car() { Id = 3, Model = null, Price = 2, LicensePlate = "AB12345" };
+		Car invalidModelEmpty = new Car() { Id = 4, Model = "", Price = 2, LicensePlate = "AB12345" };
+
+		//ValidatePriceTest()-cases
+		Car validPrice = new Car() { Id = 5, Model = "Model", Price = 2, LicensePlate = "AB12345" };
+		Car invalidPrice = new Car() { Id = 6, Model = "Model", Price = 0, LicensePlate = "AB12345" };
+
+		//ValidateLicensePlateTest()-cases
+		Car validLicensePlateMin = new Car() { Id = 7, Model = "Model", Price = 2, LicensePlate = "AB" };
+		Car validLicensePlateMax = new Car() { Id = 8, Model = "Model", Price = 2, LicensePlate = "AB12345" };
+		Car invalidLicensePlateNull = new Car() { Id = 9, Model = "Model", Price = 2, LicensePlate = null };
+		Car invalidLicensePlateLessThan = new Car() { Id = 10, Model = "Model", Price = 2, LicensePlate = "A" };
+		Car invalidLicensePlateGreaterThan = new Car() { Id = 11, Model = "Model", Price = 2, LicensePlate = "AB123456" };
+
+		//ValidateAll()-
+		Car validCar = new Car() { Id = 12, Model = "Model", Price = 2, LicensePlate = "AB12345" };
+		
+		[TestMethod()]
+		public void ToStringTest()
+		{
+			Assert.AreEqual("1 Model 2 AB12345", validToString.ToString());
+		}
+
+		[TestMethod()]
+		public void ValidateModelTest()
+		{
+			validModel.ValidateModel();
+			Assert.ThrowsException<ArgumentNullException>(() => invalidModelNull.ValidateModel());
+			Assert.ThrowsException<ArgumentException>(() => invalidModelEmpty.ValidateModel());
+		}
+
+		[TestMethod()]
+		public void ValidatePriceTest()
+		{
+			validPrice.ValidatePrice();
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidPrice.ValidatePrice());
+		}
+
+		[TestMethod()]
+		public void ValidateLicensePlateTest()
+		{
+			validLicensePlateMin.ValidateLicensePlate();
+			validLicensePlateMax.ValidateLicensePlate();
+			Assert.ThrowsException<ArgumentNullException>(() => invalidLicensePlateNull.ValidateLicensePlate());
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidLicensePlateLessThan.ValidateLicensePlate());
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidLicensePlateGreaterThan.ValidateLicensePlate());
+		}
+
+		[TestMethod()]
+		public void ValidateAllTest()
+		{
+			validCar.ValidateAll();
+
+			Assert.ThrowsException<ArgumentNullException>(() => invalidModelNull.ValidateModel());
+			Assert.ThrowsException<ArgumentException>(() => invalidModelEmpty.ValidateModel());
+
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidPrice.ValidatePrice());
+
+			Assert.ThrowsException<ArgumentNullException>(() => invalidLicensePlateNull.ValidateLicensePlate());
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidLicensePlateLessThan.ValidateLicensePlate());
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidLicensePlateGreaterThan.ValidateLicensePlate());
+		}
+	}
+}
